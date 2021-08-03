53310d9bc96e1c695e8e2237885230ed0900982e
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-03 14:13:52 +1200

    matrix code for reading table

--- matrix.py
+++ matrix.py
@@ -1,9 +1,21 @@
 class Matrix:
-    def __init___(self, matrix_string="1 2\n 3 4"):
-        self.newArray = matrix_string.split('\n').split("")
-        return self.newArray
+    def __init__(self, matrix_string):
+        self.newList = matrix_string.split('\n')
+        print(self.newList)
     def row(self, index):
-        return self.newArray[index]
-
+        testList = []
+        testRow = self.newList[index-1].split(' ')
+        for number in testRow:
+            testList.append(int(number))
+        return testList
     def column(self, index):
-        pass
+        gridLen = len(self.newList)
+        testList = []
+        for row in range(gridLen):
+            testList.append(int(self.newList[row].split(' ')[index-1]))
+        return testList
+matrix = Matrix("9 8 7\n5 3 2\n6 6 7")
+# print row at index
+print(matrix.row(1))
+# print column at index
+print(matrix.column(1))
